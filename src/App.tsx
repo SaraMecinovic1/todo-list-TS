6823c7f08841105cac4ef2eabb2e9116e96fa85d
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-09-23 12:40:05 +0200

    todo list in ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { TodoList } from "./components/todo-list";
+
+function App() {
+  return (
+    <div className="bg-slate-100 w-full min-h-screen">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo-list.tsx
@@ -0,0 +1,7 @@
+export const TodoList = () => {
+  return (
+    <div className="container bg-slate-100  ">
+      <h1 className="text-3xl font-bold text-center mb-4">To-Do App</h1>
+    </div>
+  );
+};
