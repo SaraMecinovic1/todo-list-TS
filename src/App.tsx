2901d0d5e6bd8cb90e7f223db2f65868a4a688dc
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-09-23 13:04:25 +0200

    fixing some problems...

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { TodoList } from "./components/todo-list";
+import './index.css'; // Importuj Tailwind stilove
 
 function App() {
   return (
-    <div className="bg-slate-100 w-full min-h-screen">
+    <div className="w-full min-h-screen bg-slate-100">
       <TodoList />
     </div>
   );
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,7 +1,12 @@
+import { Input } from "@mui/material";
+
 export const TodoList = () => {
   return (
-    <div className="container bg-slate-100  ">
-      <h1 className="text-3xl font-bold text-center mb-4">To-Do App</h1>
+    <div>
+      <h1>todos</h1>
+      <div>
+        <Input />
+      </div>
     </div>
   );
 };
